b7a5ade2efcbab0d88a50a885bb8e3a1a5533148
BotondProhaszka <prohaszka.bendi@gmail.com>
2021-04-03 16:03:53 +0200

    Start project

--- Skeleton/Skeleton.cpp
+++ Skeleton/Skeleton.cpp
@@ -33,16 +33,17 @@
 //=============================================================================================
 #include "framework.h"
 
-// vertex shader in GLSL: It is a Raw string (C++11) since it contains new line characters
 const char * const vertexSource = R"(
-	#version 330				// Shader 3.3
-	precision highp float;		// normal floats, makes no difference on desktop computers
+	#version 330
+	precision highp float;
 
-	uniform mat4 MVP;			// uniform variable, the Model-View-Projection transformation matrix
-	layout(location = 0) in vec2 vp;	// Varying input: vp = vertex position is expected in attrib array 0
+	uniform vec3 wLookAt, wRight, wUp;
+	layout(location = 0) in vec2 cCamWindowVertex;
+	out vec3 p;
 
 	void main() {
-		gl_Position = vec4(vp.x, vp.y, 0, 1) * MVP;		// transform vp from modeling space to normalized device space
+		gl_Position = vec4(cCamWindowVertex, 0, 1);
+		p = wLookAt + wRight * cCamWindowVertex.x + wUp * cCamWindowVertex.y;
 	}
 )";
 
@@ -51,16 +52,122 @@ const char * const fragmentSource = R"(
 	#version 330			// Shader 3.3
 	precision highp float;	// normal floats, makes no difference on desktop computers
 	
-	uniform vec3 color;		// uniform variable, the color of the primitive
-	out vec4 outColor;		// computed color of the current pixel
+	const vec3 La = vec3(0.5f, 0.6f, 0.6f);
+	const vec3 Le = vec3(0.8f, 0.8f, 0.8f);
+	const vec3 lightPosition = vec3(0.4f, 0.4f, 0.25f);
+	const vec3 ka = vec3(0.5f, 0.5f, 0.5f);
+	const float shininess = 500.0f;
+	const int maxdepth = 5;
+	const float epsilon = 0.1f;
+	
+	struct Hit {
+		float t;
+		vec3 positio, normal;
+		int mat;
+	};
+
+	struct Ray {
+		vec3 start, dir, weight;
+	}
+
+	const int objFaces = 12;
+	uniform int top;
+	uniform vec3 wEye, v[20];
+	uniform int planes[objFaces * 3];
+	uniform vec3 kd[2], ks[2], F0;
+
+	void getObjPlane(int i, float scale, out vec3 p, outvec3 normal) {
+		vec3 p1 = v[planes[3 * i] - 1], p2 = v[planes[3 * i + 1] - 1], p3 = v[planes[3 * i + 2] -1];
+		normal = cross(p2 - p1, p3 - p1);
+		if(dot(p1, normal) < 0) normal = -normal;
+		
+	Hit intersectMirascope(Ray ray, Hit hit) {
+		const float f = 0.25f;
+		const float H = 0.98f * f;
+
+
+
+
+
+
+	Hit firstIntersect(Ray ray) {
+		Hit bestHit;
+		bestHit.t = -1;
+		bestHit = intersectMirascope(ray, bestHit);
+		bestHit = intersectConvexPolyhedron(ray, bestHit, 0.02f, 0);
+		bestHit = intersectConvexPolyhedron(ray, bestHit, 1.2f, 1);
+		if(dot(ray.dir, bestHit.normal) > 0) bestHit.normal = bestHit.normal * (-1);
+		return bestHit;
+	}
+
+	vec3 trace(Ray ray) {
+		vec3 outRadiance = vec3(0, 0, 0);
+		for(int d = 0; d < maxdepth; d++) {
+			Hit hit = firstIntersect(ray);
+			if(hit.t < 0) break;
+			if(hit.mat < 2) {
+				vec3 lightdir = normalize(lightPosition - hit.position);
+				float cosTheta > 0) {
+					vec3 LeIn = Le / dot(lightPosition - hit.position, lightPosition - hit.position);
+					outRadiance += ray.weight * LeIn * kd[hit.mat] * cosTheta;
+					vec3 halfway = normalize(-ray.dir + lightdir);
+					float cosDelta = dot(hit.normal, halfway);
+					if(cosDelta > 0) outRadiance += ray.weight * LeIn * ks[hit.mat] * pow(cosDelta, shininess);
+				}
+				ray.weight *= ka;
+				break;
+			}
+		ray.weight *= F0 + (vec3(1, 1, 1) - F0) * pow(dot(-ray.dir, hit.normal), 5);
+		ray.start = hit.position + hit.normal * epsilon;
+		ray.dir = reflect(ray.dir, hit.normal);
+	}
+	outRadiance += ray.weight * La;
+	return outRadiance;
+
+
+	in vec3 p;
+	out vec4 fragmentColor;
 
 	void main() {
-		outColor = vec4(color, 1);	// computed color is the color of the primitive
+		Ray ray;
+		ray.start = wEye;
+		ray.dirr = normalize(p - wEye);
+		ray.weight = vec3(1, 1, 1);
+		fragmentColor = vec4(trace(ray), 1);
 	}
 )";
 
 GPUProgram gpuProgram; // vertex and fragment shaders
 unsigned int vao;	   // virtual world on the GPU
+
+
+struct Camera {
+	vec3 eye, lookat, right, pvup, rvup;
+	float fov = 45 * (float)M_PI / 180;
+
+	Camera() : eye(0, 1, 1), pvup(0, 0, 1), lookat(0, 0, 0) { set(); }
+	void set() {
+		vec3 w = eye - lookat;
+		float f = length(w);
+		right = normalize(cross(pvup, w)) * f * tanf(fov / 2);
+		rvup = normalize(cross(w, right)) * f * tanf(fov / 2);
+	}
+	void Animate(float t) {
+		float r = sqrtf(eye.x * eye.x + eye.y * eye.y);
+		eye = vec3(r * cos(t) + lookat.x, r * sin(t) + lookat.y, eye.z);
+		set();
+	}
+	void Step(float step) {
+		eye = normalize(eye + pvup * step) * length(eye);
+		set();
+	}
+};
+
+
+
+
+
+float F(float n, float k) { return ((n - 1) * (n - 1) + k * k) / ((n + 1) * (n + 1) + k * k); }
 
 // Initialization, create an OpenGL context
 void onInitialization() {
@@ -73,73 +180,56 @@ void onInitialization() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexCoords), vertexCoords, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
-	shader.create(vertexSource,  fragmentSource, "fragmentColor");
+	shader.create(vertexSource, fragmentSource, "fragmentColor");
 	shader.setUniform(1, "top");
 
 	const float g = 0.618f, G = 1.618f;
+	std::vector<vec3> v = {
+		vec3(0, g, G), vec3(0, -g, G), vec3(0, -g, -G), vec3(0, g, -G), vec3(G, 0, g), vec3(-G, 0, g), vec3(-G, 0, -g), vec3(G, 0, -g),
+		vec3(-g, -G, 0), vec3(g, -G, 0), vec3(1, 1, 1), vec3(-1, 1, 1), vec3(-1, -1, 1), vec3(1, -1, -1), vec3(-1, -1, -1)
+	};
+
+	for (int i = 0; i < v.size(); i++) shader.setUniform(v[i], "v[" + std::to_string(i) + "]");
+
+	std::vector<int> planes = {
+		1, 2, 16,	1, 13, 9,	1, 14, 6,	2, 15, 11,	3, 4, 18,	3, 17, 12,	3, 20, 7,	19, 10, 9,	16, 12, 17,	5, 8, 18,	14
+	};
+	for (int i = 0; i < planes.size(); i++) shader.setUniform(planes[i], "planes[" + std::to_string(i) + "]");
+
+	shader.setUniform(vec3(0.1f, 0.2f, 0.3f), "kd[0]");
+	shader.setUniform(vec3(1.5f, 0.6f, 0.4f), "kd[1]");
+	shader.setUniform(vec3(5, 5, 5), "ks[0]");
+	shader.setUniform(vec3(1, 1, 1), "ks[0]");
+	shader.setUniform(vec3(F(0.17, 3.1), F(0.35, 2.7), F(1.5, 1.9)), "F0");
 
 }
 
 // Window has become invalid: Redraw
 void onDisplay() {
-	glClearColor(0, 0, 0, 0);     // background color
-	glClear(GL_COLOR_BUFFER_BIT); // clear frame buffer
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT |GL_DEPTH_BUFFER_BIT);
+	shader.setUniform(camera.eye, "wEye");
+	shader.setUniform(camera.lookat, "wLookAt");
+	shader.setUniform(camera.right, "wRight");
+	shader.setUniform(camera.rvup, "wUp");
+	glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
-	// Set color to (0, 1, 0) = green
-	int location = glGetUniformLocation(gpuProgram.getId(), "color");
-	glUniform3f(location, 0.0f, 1.0f, 0.0f); // 3 floats
-
-	float MVPtransf[4][4] = { 1, 0, 0, 0,    // MVP matrix, 
-							  0, 1, 0, 0,    // row-major!
-							  0, 0, 1, 0,
-							  0, 0, 0, 1 };
-
-	location = glGetUniformLocation(gpuProgram.getId(), "MVP");	// Get the GPU location of uniform variable MVP
-	glUniformMatrix4fv(location, 1, GL_TRUE, &MVPtransf[0][0]);	// Load a 4x4 row-major float matrix to the specified location
-
-	glBindVertexArray(vao);  // Draw call
-	glDrawArrays(GL_TRIANGLES, 0 /*startIdx*/, 3 /*# Elements*/);
-
-	glutSwapBuffers(); // exchange buffers for double buffering
+	glutSwapBuffers();
 }
 
-// Key of ASCII code pressed
 void onKeyboard(unsigned char key, int pX, int pY) {
-	if (key == 'd') glutPostRedisplay();         // if d, invalidate display, i.e. redraw
+	if (key == 't') shader.setUniform(1, "top");
+	if (key == 'T') shader.setUniform(0, "top");
+	if (key == 'f') camera.Step(0.1f);
+	if (key == 'F') camera.Step(-0.1f);
+	if (key == 'a') animate = !animate;
 }
 
-// Key of ASCII code released
-void onKeyboardUp(unsigned char key, int pX, int pY) {
-}
+void onKeyboardUp(unsigned char key, int pX, int pY) {}
+void onMouseMotion(int pX, int pY) {}
+void onMouse(int button, int state, int pX, int pY) {}
 
-// Move mouse with key pressed
-void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
-	// Convert to normalized device space
-	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
-	float cY = 1.0f - 2.0f * pY / windowHeight;
-	printf("Mouse moved to (%3.2f, %3.2f)\n", cX, cY);
-}
-
-// Mouse click event
-void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
-	// Convert to normalized device space
-	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
-	float cY = 1.0f - 2.0f * pY / windowHeight;
-
-	char * buttonStat;
-	switch (state) {
-	case GLUT_DOWN: buttonStat = "pressed"; break;
-	case GLUT_UP:   buttonStat = "released"; break;
-	}
-
-	switch (button) {
-	case GLUT_LEFT_BUTTON:   printf("Left button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);   break;
-	case GLUT_MIDDLE_BUTTON: printf("Middle button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY); break;
-	case GLUT_RIGHT_BUTTON:  printf("Right button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);  break;
-	}
-}
-
-// Idle event indicating that some time elapsed: do animation here
 void onIdle() {
-	long time = glutGet(GLUT_ELAPSED_TIME); // elapsed time since the start of the program
+	if (animate) camera.Animate(glutGet(GLUT_ELAPSED_TIME) / 1000.0f);
+	glutPostRedisplay();
 }
